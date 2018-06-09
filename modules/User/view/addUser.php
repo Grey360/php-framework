fbece199e995eda72552eda66c3769f1734e8daa
Gailor <gailor.petemoya@capgemini.com>
2018-06-09 13:43:45 +0200

    SQL

--- modules/User/view/addUser.php
+++ modules/User/view/addUser.php
@@ -1,8 +1,13 @@
 <div>
     <form action="/user/add" method="post">
-        <input type="text" name="street" id="street">
-        <input type="number" name="number" id="number">
-        <input type="text" name="country" id="country">
-        <input type="text" name="city" id="city">
+        <label for="street">Street</label>
+        <input type="text" name="street" id="street" placeholder="Street...">
+        <label for="number">Number</label>
+        <input type="number" name="number" id="number" placeholder="Street number...">
+        <label for="country">Country</label>
+        <input type="text" name="country" id="country" placeholder="Country...">
+        <label for="city">city</label>
+        <input type="text" name="city" id="city" placeholder="City...">
+        <input type="submit" value="Submit">
     </form>
 </div>
